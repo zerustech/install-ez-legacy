42e14c6325858b767af4041d9d9d99a6dfb1f331
Michael Lee <michael.lee@zerustech.com>
2015-12-02 19:31:31 +0800

    Optimize script

--- scripts/apply-legacy-patches.sh
+++ scripts/apply-legacy-patches.sh
@@ -6,8 +6,6 @@ SCRIPT_PATH=`( cd "$SCRIPT_PATH" && pwd )`
 
 cd "$SCRIPT_PATH/../.."
 
-echo `pwd`
-
 patch -p1 < $prefix/patches/composer-composer.json.1.patch
 patch -p1 < $prefix/patches/config-config.yml.2.patch
 patch -p1 < $prefix/patches/config-routing.yml.2.patch
