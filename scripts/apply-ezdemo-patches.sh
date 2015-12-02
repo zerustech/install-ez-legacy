b99e67bc38ca8b3c04fb79c6222e29c0b97f85c5
Michael Lee <michael.lee@zerustech.com>
2015-12-02 19:32:13 +0800

    Optimize scripts

--- scripts/apply-ezdemo-patches.sh
+++ scripts/apply-ezdemo-patches.sh
@@ -6,8 +6,6 @@ SCRIPT_PATH=`( cd "$SCRIPT_PATH" && pwd )`
 
 cd "$SCRIPT_PATH/../.."
 
-echo `pwd`
-
 patch -p1 < $prefix/patches/config-config.yml.1.patch
 patch -p1 < $prefix/patches/config-routing.yml.1.patch
 patch -p1 < $prefix/patches/ez-EzPublishKernel.php.1.patch
