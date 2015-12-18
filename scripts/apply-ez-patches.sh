f5f80c89c45ac164253473d7ea27ae52b7168c37
Michael Lee <michael.lee@zerustech.com>
2015-12-18 15:10:41 +0800

    Added apply-ez-patches.sh

---
+++ scripts/apply-ez-patches.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+prefix=install-ez-legacy
+SCRIPT_PATH=`dirname "$0"`
+SCRIPT_PATH=`( cd "$SCRIPT_PATH" && pwd )`
+
+cd "$SCRIPT_PATH/../.."
+
+patch -p1 < $prefix/patches/ez-siteaccessmatchlistener.php.1.patch
+
+cd $SCRIPT_PATH
