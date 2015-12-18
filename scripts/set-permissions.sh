c652f2e2526c596025186dd4789fdcaef13c2630
Michael Lee <michael.lee@zerustech.com>
2015-12-18 15:32:09 +0800

    Fixed permission issues

--- scripts/set-permissions.sh
+++ scripts/set-permissions.sh
@@ -7,30 +7,30 @@ SCRIPT_PATH=`( cd "$SCRIPT_PATH" && pwd )`
 
 cd "$SCRIPT_PATH/../.."
 
-sudo chown -R $WEB_USER:$WEB_USER_GROUP ezpublish web
+chown -R $WEB_USER:$WEB_USER_GROUP ezpublish web
 
-sudo chown -R $WEB_USER:$WEB_USER_GROUP ezpublish_legacy/var
+chown -R $WEB_USER:$WEB_USER_GROUP ezpublish_legacy/var
 
 chmod -R 777 ~/.composer/cache
 
 for f in `find {ezpublish/{cache,logs,config,sessions},web} -type d`
 do
-    sudo chmod -R 755 $f
+    chmod -R 755 $f
 done
 
 for f in `find ezpublish_legacy/{design,extension,settings,var} -type d`
 do
-    sudo chmod -R 755 $f
+    chmod -R 755 $f
 done
 
 for f in `find {ezpublish/{cache,logs,config,sessions},web} -type f`
 do
-    sudo chmod -R 664 $f
+    chmod -R 664 $f
 done
 
 for f in `find ezpublish_legacy/{design,extension,settings,var} -type f`
 do
-    sudo chmod -R 664 $f
+    chmod -R 664 $f
 done
 
 cd $SCRIPT_PATH
